05ca28112606da57598284bc410d3c1a6c1cebd4
Chris Langhout <clanghout@bol.com>
2022-12-07 11:56:17 +0100

    clean day 7

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+
 use regex::Regex;
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -10,55 +11,46 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("result: {}", part1);
 
-    let part2 = calc_part2(&    input_numbers);
+    let part2 = calc_part2(&input_numbers);
 
     println!("result: {}", part2);
 
     Ok(())
 }
 
-fn parse_input(contents: &str) -> Vec<&str> {
-    contents
-        .trim()
-        .lines()
-        .collect()
-}
-
-fn calc_part1(input: &[&str]) -> usize {
-    let mut input = input.clone().iter();
-    input.next(); // skip first line, we are in folder "/"
-    let mut cur = vec!["/"];
+fn parse_input(contents: &str) -> HashMap<String, usize> {
+    let mut cur = vec![];
     let mut folders: HashMap<String, usize> = HashMap::new();
     let re = Regex::new(r"(\d+) .*").unwrap();
 
-    input
-        .for_each(|&line|
+    contents
+        .trim()
+        .lines()
+        .for_each(|line|
             match line {
-                _ if line.starts_with("$ cd ..") => {cur.pop();}
+                _ if line.starts_with("$ cd ..") => { cur.pop(); }
                 _ if line.starts_with("$ cd") => {
                     cur.push(line.split_at(5).1);
-                    // println!("current folder: {:?}", cur);
                 }
                 _ if line.starts_with("$ ls") || line.starts_with("dir") => {
                     // do nothing
-                    }
-                _ => cur.iter().enumerate().for_each(|(i,_)| {
-                    // println!("we have a line with size: {}",line);
-                    let key = cur.iter().take(i+1).map(|a|*a).collect::<Vec<&str>>().concat();
+                }
+                _ => cur.iter().enumerate().for_each(|(i, _)| {
+                    let key = cur.iter().take(i + 1).map(|a| *a).collect::<Vec<&str>>().concat();
                     let size = match re.captures(line) {
                         Some(cap) => cap[1].parse().unwrap(),
                         None => panic!("Invalid move string {}", line),
                     };
-                    // println!("adding size: {} to key: {}",size, key);
                     folders.entry(key).and_modify(|total_size| *total_size += size).or_insert(size);
                 })
             }
         );
+    folders
+}
 
-    // println!("{:?}", folders);
-
+fn calc_part1(input: &HashMap<String, usize>) -> usize {
     let mut res = 0usize;
-    for (_, &size) in &folders {
+    for (_, &size) in input {
         if size < 100_000usize {
             res += size;
         }
@@ -66,48 +58,15 @@ fn calc_part1(input: &[&str]) -> usize {
     res
 }
 
-
-fn calc_part2(input: &[&str]) -> usize {
-    let mut input = input.clone().iter();
-    input.next(); // skip first line, we are in folder "/"
-    let mut cur = vec!["/"];
-    let mut folders: HashMap<String, usize> = HashMap::new();
-    let re = Regex::new(r"(\d+) .*").unwrap();
-
-    input
-        .for_each(|&line|
-            match line {
-                _ if line.starts_with("$ cd ..") => {cur.pop();}
-                _ if line.starts_with("$ cd") => {
-                    cur.push(line.split_at(5).1);
-                    // println!("current folder: {:?}", cur);
-                }
-                _ if line.starts_with("$ ls") || line.starts_with("dir") => {
-                    // do nothing
-                }
-                _ => cur.iter().enumerate().for_each(|(i,_)| {
-                    // println!("we have a line with size: {}",line);
-                    let key = cur.iter().take(i+1).map(|a|*a).collect::<Vec<&str>>().concat();
-                    let size = match re.captures(line) {
-                        Some(cap) => cap[1].parse().unwrap(),
-                        None => panic!("Invalid move string {}", line),
-                    };
-                    // println!("adding size: {} to key: {}",size, key);
-                    folders.entry(key).and_modify(|total_size| *total_size += size).or_insert(size);
-                })
-            }
-        );
-
-    // println!("{:?}", folders);
-
-    let free_space = 70000000 - folders.get("/").unwrap();
+fn calc_part2(input: &HashMap<String, usize>) -> usize {
+    let free_space = 70000000 - input.get("/").unwrap();
     let minimal_delete_size = 30000000 - free_space;
 
-    folders
+    input
         .iter()
-        .filter_map(|(_,&size)| if size > minimal_delete_size { Option::Some(size) }else {Option::None})
-        .min().unwrap()
-
+        .filter_map(|(_, &size)| if size > minimal_delete_size { Some(size) } else { None })
+        .min()
+        .unwrap()
 }
 
 #[test]
